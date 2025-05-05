c097a1e7de49f97a8f8f985d7fd1e37e180f69e1
Arul <arul03sekar@gmail.com>
2025-05-05 11:10:44 +0530

    backend to pull

--- franchise_frontend/src/main.jsx
+++ franchise_frontend/src/main.jsx
@@ -1,9 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import { Provider } from "react-redux"
+import {Provider} from "react-redux"
 import App from './App.jsx'
-import store from './Redux/store/index.jsx'
-
-import  BrowserRouter  from 'react-router-dom'
+import store from './Redux/Store/Index.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
 <Provider store={store}>
