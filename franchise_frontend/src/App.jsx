8d01aeeae6a1b223787c28c36287696cc3d74ca7
Aswinievanjeline <evanjelineaswini@gmail.com>
2025-05-10 11:38:13 +0530

    ss

--- franchise_frontend/src/App.jsx
+++ franchise_frontend/src/App.jsx
@@ -9,6 +9,11 @@ import './App.css'
 import HomeBannerSec from './Components/HomePage_BannerSection/HomeBannerSec'
 import TopBrandVdoSec from "./Components/HomePage_VideoSection/TopBrandVdoSec";
 import BrandViewPage from "./Pages/BrandViewPage/BrandViewPage";
+import TermsCondition from './Components/QuickLinks/TermsCondition.jsx'
+import Faq from './Components/QuickLinks/Faq.jsx'
+import Help from './Components/QuickLinks/Help.jsx'
+import ContactUs from './Components/QuickLinks/ContactUs.jsx'
+import AboutUs from './Components/QuickLinks/AboutUs.jsx'
 
 function App() {
 
@@ -19,6 +24,11 @@ function App() {
 <Routes>
 <Route path='/' element={<HomeBannerSec/>}/>
 <Route path='/brandViewPage' element={<BrandViewPage/>}/>
+<Route path ='/aboutuspage' element={<AboutUs/>}/>
+       <Route path='/contactuspage' element={<ContactUs/>}/>
+         <Route path='/help' element={<Help/>}/>
+         <Route path='/faq' element={<Faq/>}/>
+        <Route path='/termscondition' element={<TermsCondition/>}/>
 </Routes>
 </>
     
