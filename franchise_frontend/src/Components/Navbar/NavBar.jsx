c904ffdf847ac05b1c2cc30d5496f94113bf6bac
AyanMrFranchies <ayankalaibabaateam@gmail.com>
2025-06-02 18:58:51 +0530

    new update in investor dashboard delete api created successfully

--- franchise_frontend/src/Components/Navbar/NavBar.jsx
+++ franchise_frontend/src/Components/Navbar/NavBar.jsx
@@ -105,7 +105,7 @@ function Navbar() {
       const response = await axios.post(
         // `https://franchise-backend-wgp6.onrender.com/api/v1/logout/${ID}`,
         `http://localhost:5000/api/v1/logout/${ID}`,
-        `https://franchise-backend-wgp6.onrender.com/api/v1/logout/${ID}`,
+        // `https://franchise-backend-wgp6.onrender.com/api/v1/logout/${ID}`,
         {},
         {
           headers: {
