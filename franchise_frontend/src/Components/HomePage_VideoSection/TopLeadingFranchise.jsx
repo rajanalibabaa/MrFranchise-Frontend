240291906801d2cf23f4c9e5a25a0bcca7afb6d3
rajanalibabaa <rajanalibabaa@gmail.com>
2025-07-02 16:24:14 +0530

    rajan

--- franchise_frontend/src/Components/HomePage_VideoSection/TopLeadingFranchise.jsx
+++ franchise_frontend/src/Components/HomePage_VideoSection/TopLeadingFranchise.jsx
@@ -28,7 +28,6 @@ import { useDispatch, useSelector } from "react-redux";
 import LoginPage from "../../Pages/LoginPage/LoginPage";
 import {
   fetchBrands,
-  openBrandDialog,
   toggleLikeBrand,
 } from "../../Redux/Slices/brandSlice";
 import BrandDetailsDialog from "../../Pages/AllCategoryPage/BrandDetailsDialog";
