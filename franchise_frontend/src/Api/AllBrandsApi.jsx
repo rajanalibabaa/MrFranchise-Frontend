337299167d03fb2ede1f760aacf22dccb6e257ab
Arul <arul03sekar@gmail.com>
2025-07-22 14:23:35 +0530

    push

---
+++ franchise_frontend/src/Api/AllBrandsApi.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { fetchAllBrands } from '../Redux/Slices/AllBrandSlice/AllBrandSlice';
+
+const AllBrandsApi = () => {
+    
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        const fetchBrands = async () => {
+            try {
+                const token = localStorage.getItem("accessToken");
+                const id = localStorage?.getItem("investorUUID") || localStorage?.getItem("brandUUID");
+                let response;
+
+                if (!token) {
+                    response = await axios.get(
+                        "http://localhost:5000/api/v1/brandlisting/getAllBrandListing",
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                        }
+                    );
+                } else {
+                    response = await axios.get(
+                        `http://localhost:5000/api/v1/like/favbrands/getAllLikedAndUnlikedBrand/${id}`,
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization: `Bearer ${token}`,
+                            },
+                        }
+                    );
+                }
+
+                console.log("All brand new data :",response.data.data)
+                
+                // setBrands(response.data.data);
+
+                if (response.data) {
+                    dispatch(fetchAllBrands(response.data.data))
+                }
+            } catch (err) {
+                // setError(err.message || "Failed to fetch brands");
+                console.error("Error fetching brands:", err);
+            }
+        };
+
+        fetchBrands();
+    }, [dispatch]);
+
+    return (
+        <div>
+           
+        </div>
+    );
+};
+
+export default AllBrandsApi;
