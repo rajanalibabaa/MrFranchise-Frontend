920149f3074f97b4091f7f8d788a94dbd5d14a6c
rajanalibabaa <rajanalibabaa@gmail.com>
2025-07-23 15:33:05 +0530

    all

--- franchise_frontend/src/Components/Investor_Profile_Component/ManageProfile.jsx
+++ franchise_frontend/src/Components/Investor_Profile_Component/ManageProfile.jsx
@@ -209,7 +209,7 @@ const ManageProfile = () => {
       try {
         setLoading(true);
         const response = await axios.get(
-          `https://mrfranchisebackend.mrfranchise.in/investor/getInvestorByUUID/${investorUUID}`,
+          `https://mrfranchisebackend.mrfranchise.in/api/v1/investor/getInvestorByUUID/${investorUUID}`,
           {
             headers: {
               "Content-Type": "application/json",
@@ -297,7 +297,7 @@ const ManageProfile = () => {
 
     try {
       const response = await axios.post(
-        "https://mrfranchisebackend.mrfranchise.in/otp/existingEmailOTP",
+        "https://mrfranchisebackend.mrfranchise.in/api/v1/otp/existingEmailOTP",
         { email: investorData.email },
         { headers: { "Content-Type": "application/json" } }
       );
@@ -342,7 +342,7 @@ const ManageProfile = () => {
 
     try {
       const response = await axios.post(
-        "https://mrfranchisebackend.mrfranchise.in/otp/verifyExistingEmailOTP",
+        "https://mrfranchisebackend.mrfranchise.in/api/v1/otp/verifyExistingEmailOTP",
         {
           email: investorData.email,
           verifyOTP: otp,
